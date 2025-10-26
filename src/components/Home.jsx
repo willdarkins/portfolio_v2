186aed919cee9420a509403a1cff056cda861612
willdarkins <will.darkins@adcellerant.com>
2025-10-26 13:07:09 -0700

    added profile image and began work on home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import profileImage from '../../public/profile_image.jpeg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div id= 'home' className='flex min-h-screen w-full items-center justify-center'>
+
+      <div className='flex flex-col items-center justify-center gap-8 p-5 text-center'>
+        <img src={profileImage} alt="" className='w-[250px] sm:w-[300px] rounded-full ' />
+      </div>
+    </div>
   )
 }
 
