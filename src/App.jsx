7ede734de28d07756659ca544fa6fd29898837ad
willdarkins <will.darkins@adcellerant.com>
2025-10-25 22:58:45 -0700

    linked components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import HeaderLeft from './components/HeaderLeft';
+import Home from './components/Home';
+import Experience from './components/Experience';
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
 function App() {
 
   return (
