96f3beaf412cc69d84b6511712912d2ca7f80188
willdarkins <will.darkins@adcellerant.com>
2025-11-08 09:21:36 -0800

    updated experienceData.js with new key value naming

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,37 +2,37 @@
 
 const experienceData = [
   {
-    title: "Experience 1",
+    company: "Experience 1",
     jobTitle: "Frontend Engineer",
     description:
       "Ut ullamco ad id sit. Commodo occaecat officia ad veniam ea consectetur id laborum mollit velit do eiusmod aute adipisicing.",
   },
   {
-    title: "Experience 2",
+    company: "Experience 2",
     jobTitle: "QA Automation Engineer",
     description:
       "Mollit laborum nisi dolor exercitation pariatur aliquip veniam ut fugiat irure irure reprehenderit.",
   },
   {
-    title: "Experience 3",
+    company: "Experience 3",
     jobTitle: "Tech Lead",
     description:
       "Cillum id quis ipsum anim ea ea irure mollit. Sunt laborum aute anim elit incididunt culpa.",
   },
   {
-    title: "Experience 4",
+    company: "Experience 4",
     jobTitle: "DevOps Specialist",
     description:
       "Ex labore sint laboris magna officia pariatur deserunt incididunt veniam ad voluptate elit pariatur. pariatur deserunt incididunt veniam ad voluptate elit pariatur",
   },
   {
-    title: "Experience 5",
+    company: "Experience 5",
     jobTitle: "Product Manager",
     description:
       "Aliqua duis dolore tempor sint deserunt laboris anim ex eiusmod exercitation culpa elit nulla pariatur.",
   },
   {
-    title: "Experience 6",
+    company: "Experience 6",
     jobTitle: "Senior QA Engineer",
     description:
       "Voluptate labore anim deserunt consectetur qui laboris in non ea ut.",
