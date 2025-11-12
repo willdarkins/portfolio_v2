5c993ea86ff1596101fc335eaa6d6834c7cc031d
willdarkins <will.darkins@adcellerant.com>
2025-11-12 09:47:51 -0800

    added project data and solidified styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,13 @@
 const Projects = () => {
   return (
-    <div id="projects" className="min-h-screen w-full flex flex-col items-center justify-center gap-20 p-10 md:p-16 xl:p-32 ">
+    <div id="projects" className="min-h-screen w-full flex flex-col items-center justify-center gap-20 p-10 md:p-16 xl:px-32 ">
       <h1 className="text-center text-5xl font-light ">Projects</h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 ">
         <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-sky-950 rounded-lg hover:bg-sky-50 ">
-          <h1 className="text-3xl font-semibold ">Project 1</h1>
-          <h3 className="text-xl text-gray-700 ">Tech Stack</h3>
-          <p className="text-sm text-gray-600">Aliqua aliqua aliquip veniam id est quis est elit labore irure id aliquip cupidatat. In qui qui magna ipsum nostrud id consequat mollit ullamco minim irure magna est ex. Ex pariatur commodo laborum irure laboris. Enim aliqua occaecat nisi quis commodo.</p>
+          <h1 className="project-headers">Project 1</h1>
+          <h3 className="project-sub-headers">Tech Stack</h3>
+          <p className="project-body-text">Aliqua aliqua aliquip veniam id est quis est elit labore irure id aliquip cupidatat. In qui qui magna ipsum nostrud id consequat mollit ullamco minim irure magna est ex. Ex pariatur commodo laborum irure laboris. Enim aliqua occaecat nisi quis commodo.</p>
         </div>
       </div>
     </div>
---
+++ src/data/projectData.js
@@ -0,0 +1,40 @@
+const projectData = [
+  {
+    projectTitle: "Project 1",
+    techStack: "Tech Stack",
+    description:
+      "Ut ullamco ad id sit. Commodo occaecat officia ad veniam ea consectetur id laborum mollit velit do eiusmod aute adipisicing.",
+  },
+  {
+    projectTitle: "Project 2",
+    techStack: "Tech Stack",
+    description:
+      "Mollit laborum nisi dolor exercitation pariatur aliquip veniam ut fugiat irure irure reprehenderit.",
+  },
+  {
+    projectTitle: "Project 3",
+    techStack: "Tech Stack",
+    description:
+      "Cillum id quis ipsum anim ea ea irure mollit. Sunt laborum aute anim elit incididunt culpa.",
+  },
+  {
+    projectTitle: "Project 4",
+    techStack: "Tech Stack",
+    description:
+      "Ex labore sint laboris magna officia pariatur deserunt incididunt veniam ad voluptate elit pariatur. pariatur deserunt incididunt veniam ad voluptate elit pariatur",
+  },
+  {
+    projectTitle: "Project 5",
+    techStack: "Tech Stack",
+    description:
+      "Aliqua duis dolore tempor sint deserunt laboris anim ex eiusmod exercitation culpa elit nulla pariatur.",
+  },
+  {
+    projectTitle: "Project 6",
+    techStack: "Tech Stack",
+    description:
+      "Voluptate labore anim deserunt consectetur qui laboris in non ea ut.",
+  },
+];
+
+export default projectData;
