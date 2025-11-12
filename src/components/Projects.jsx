8ca98a326a5e4e7cadc1aa2ed1f814609b79bdc0
willdarkins <will.darkins@adcellerant.com>
2025-11-12 06:52:45 -0800

    initial styles for container and header

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div id="projects" className="min-h-screen w-full flex flex-col items-center justify-center gap-20 p-10 md:p-16 xl:p-32 ">
+      <h1 className="text-center text-5xl font-light ">Projects</h1>
+    </div>
   )
 }
 
