6d82967724b6eed2fd776f473e8b618a3140efaf
willdarkins <will.darkins@adcellerant.com>
2025-11-12 12:39:04 -0800

    added basic component structure and installed MUI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div id='contact' className='felx min-h-screen w-full flex-col items-center justify-center gap-16 p-8 '>
+      <h1 className='text-center text-6xl font-light text-sky-950'>Get in Touch</h1>
+      
+    </div>
   )
 }
 
